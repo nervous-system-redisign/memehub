86d684d43bddcee64ed04ded75e01e7d11c9675a
rocket322 <jacob.papiez@hotmail.com>
2017-04-10 22:52:01 -0700

    Database
    created database for accounts

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
-<html>
-	<head>
-		<title>Home</title>
-	</head>
-	
-	<body>
-		<h1>
-			Welcome to memehub
-		</h1>
-	</body>
-</html>
+<?php
+	    
+$servername = "ftp.gear.host";
+$username = "memehub\$memehub";
+$password = "KvhFBGA4QkdisgNDa2SL1jllvPRJ0twXZ5zscijbkHba4aqrKuSboDPPF8ia";
+$dbname = "memehubaccounts;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+} 
+		
+$sql = "CREATE TABLE Accounts(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+password VARCHAR(30) NOT NULL,
+email VARCHAR(30) NOT NULL,
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+	echo "Table Accounts Created Successfully";
+} else {
+	echo "Error Creating table: " . $conn->error;
+}
+?>

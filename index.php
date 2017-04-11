74a034c8066fab9a9ef05959fa6a44fd7cb6cf9f
rocket322 <jacob.papiez@hotmail.com>
2017-04-10 23:00:20 -0700

    line

--- index.php
+++ index.php
@@ -1,28 +1,13 @@
-<?php
-	    
-$servername = "ftp.gear.host";
-$username = "memehub\$memehub";
-$password = "KvhFBGA4QkdisgNDa2SL1jllvPRJ0twXZ5zscijbkHba4aqrKuSboDPPF8ia";
-$dbname = "memehubaccounts;
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-} 
-		
-$sql = "CREATE TABLE Accounts(
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(30) NOT NULL,
-password VARCHAR(30) NOT NULL,
-email VARCHAR(30) NOT NULL,
-reg_date TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-	echo "Table Accounts Created Successfully";
-} else {
-	echo "Error Creating table: " . $conn->error;
-}
-?>
+<html>
+	<head>
+		<title>
+			Home
+		</title>
+	</head>
+	
+	<body>
+		</h1 align="center">
+			MemeHub
+		</h1>
+	</body>
+</html>
